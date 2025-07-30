1e547aa4b0ff60b05d96ed7e09cb1d1218305a43
Sethu-madhav <30382594+Sethu-madhav@users.noreply.github.com>
2025-07-30 13:22:18 -0400

    Refactor train_gpt2.py to introduce GPTConfig class and update attention mechanisms, modify play.ipynb for compatibility with new model structure and improve output formatting.

--- train_gpt2.py
+++ train_gpt2.py
@@ -4,23 +4,24 @@ import torch
 import torch.nn as nn
 from torch.nn import functional as F
 
-class MLP(nn.Module):
-    def __init__(self, config):
-        super().__init__()
-        self.c_fc = nn.Linear(config.n_embd, 4 * config.n_embd)
-        self.gelu = nn.GELU(approximate="tanh")
-        self.c_proj = nn.Linear(4 * config.n_embd, config.n_embd)
-    
-    def forward(self, x):
-        return self.c_proj(self.gelu(self.c_fc(x)))
+# ------------------------------------
 
-class CasualSelfAttention(nn.Module):
-    def __init__(self, config):
+@dataclass
+class GPTConfig:
+    block_size: int = 1024  # max sequence length
+    vocab_size: int = 50257 # number of tokens: 50,000 BPE merges + 256 bytes tokens + 1 <|endofturn|>  
+    n_layer: int = 12       # number of transformer blocks 
+    n_head: int = 12        # number of heads inside a block 
+    n_embd: int = 768       # embedding dimension      
+
+class CausalSelfAttention(nn.Module):
+
+    def __init__(self, config: GPTConfig) :
         super().__init__()
         assert config.n_embd % config.n_head == 0
         # key, query, value projections for all heads, but in a batch
         self.c_attn = nn.Linear(config.n_embd, 3 * config.n_embd)
-        # output projection 
+        # output projection
         self.c_proj = nn.Linear(config.n_embd, config.n_embd)
         # regularization
         self.n_head = config.n_head
@@ -28,53 +29,66 @@ class CasualSelfAttention(nn.Module):
         # not really a 'bias', more of a mask, but following the OpenAI/HF naming though
         self.register_buffer("bias", torch.tril(torch.ones(config.block_size, config.block_size))
                              .view(1, 1, config.block_size, config.block_size))
-        
-        def forward(self, x):
-            B, T, C = x.size() # batch_size, sequence_length, embedding dimensionality (n_embd)
-            # calculate query, key, values for all heads in batch and move head forward to be the batch
-            # nh is the no.of heads, hs is "head_size", and C (number of channels) = nh * hs
-            # e.g. in GPT-2 (124M), n_head=12, hs=64, so nh*hs=768 channels in the Transformer
-            qkv = self.c_attn(x)
-            q, k, v = qkv.split(self.n_embd, dim=2)
-            k = k.view(B, T, self.n_head, C // self.n_head).transpose(1, 2) # (B, nh, T, hs)
-            q = q.view(B, T, self.n_head, C // self.n_head).transpose(1, 2)
-            v = v.view(B, T, self.n_head, C // self.n_head).transpose(1, 2)
-            # attention (materializes the large (T,T) matrix for all the queries and keys)
-            att = (q @ k.transpose(-2, -1)) * (1.0 / math.sqrt(k.size(-1)))
-            att = att.masked_fill(self.bias[: , :, :T, :T] == 0, float('-inf'))
-            att = F.softmax(att, dim=-1)
-            y = att @ v # (B, nh, T, T) x (B, nh, T, hs) -> (B, nh, T, hs)
-            y = y.transpose(1, 2).contigous().view(B, T, C) # reassemble all the heads side by side
-            # output projection 
-            y = self.c_proj(y)
-            return y
+    
+    def forward(self, x):
+        B, T, C = x.size() # batch size, sequence length, embedding dimensionality (n_embd)
+        # calculate query, key, value for all heads in batch and move head forward to be the batch
+        # nh is "number of heads", hs is "head size", and C (number of channels) = nh * hs
+        # e.g. in GPT-2 (124M),  n_head=12, hs=64, so nh*hs=C=768 channels in transformer
+        qkv = self.c_attn(x)
+        q, k, v = qkv.split(self.n_embd, dim=2)
+        k = k.view(B, T, self.n_head, C // self.n_head).transpose(1, 2) # (B, nh, T, hs)
+        q = q.view(B, T, self.n_head, C // self.n_head).transpose(1, 2) 
+        v = v.view(B, T, self.n_head, C // self.n_head).transpose(1, 2)
+        # attention {materializes the large (T,T) matrix for all the queries and keys}
+        att = (q @ k.transpose(-2, -1)) * (1.0 / math.sqrt(k.size(-1)))
+        att = att.masked_fill(self.bias[:,:,:T,:T] == 0, float('-inf'))
+        att = F.softmax(att, dim=-1)
+        y = att @ v # (B, nh, T, T) x (B, nh, T, hs) -> (B, nh, T, hs)
+        y = y.transpose(1, 2).contiguous().view(B, T, C) # re-assemble all head outputs side by side
+        # output projection
+        y = self.c_proj(y)
+        return y
 
+
+class MLP(nn.Module):
+
+    def __init__(self, config: GPTConfig) :
+        super().__init__()
+        self.c_fc = nn.Linear(config.n_embd, 4 * config.n_embd)
+        self.gelu = nn.GELU(approximate='tanh')
+        self.c_proj = nn.Linear(4 * config.n_embd, config.n_embd)
+    
+    def forward(self, x):
+        x = self.c_fc(x)
+        x = self.gelu(x)
+        x = self.c_proj(x)
+        return x
 
 class Block(nn.Module):
-    def __init__(self, config):
+
+    def __init__(self, config: GPTConfig) :
         super().__init__()
         self.ln_1 = nn.LayerNorm(config.n_embd)
-        self.attn = CasualSelfAttention(config)
+        self.attn = CausalSelfAttention(config)
         self.ln_2 = nn.LayerNorm(config.n_embd)
         self.mlp = MLP(config)
-
-@dataclass
-class GPTConfig:
-    block_size: int = 256
-    vocab_size: int = 65
-    n_layer: int = 6
-    n_head: int = 6 
-    n_embd: int = 384 
+    
+    def forward(self, x):
+        x = x + self.attn(self.ln_1(x))
+        x = x + self.mlp(self.ln_2(x))
+        return x
 
 class GPT(nn.Module):
-    def __init__(self, config):
+
+    def __init__(self, config: GPTConfig):
         super().__init__()
         self.config = config
 
-        self.transfomer = nn.ModuleDict(dict(
+        self.transformer = nn.ModuleDict(dict(
             wte = nn.Embedding(config.vocab_size, config.n_embd),
             wpe = nn.Embedding(config.block_size, config.n_embd),
-            h = nn.ModuleList([Block(config) for _ in range(config.layer)]),
+            h = nn.ModuleList([Block(config) for _ in range(config.n_layer)]),
             ln_f = nn.LayerNorm(config.n_embd),
         ))
         self.lm_head = nn.Linear(config.n_embd, config.vocab_size, bias=False)
