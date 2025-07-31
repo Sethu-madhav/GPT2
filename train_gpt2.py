8faea292a66250607c7f1f253f72c8fa3f1b133f
Sethu-madhav <30382594+Sethu-madhav@users.noreply.github.com>
2025-07-30 20:27:18 -0400

    Add device assignment and text generation logic to train_gpt2.py, including token encoding and top-k sampling for generating sequences.

--- train_gpt2.py
+++ train_gpt2.py
@@ -110,7 +110,6 @@ class GPT(nn.Module):
         logits = self.lm_head(x) # (B, T, vocab_size)
         return logits
 
-
     @classmethod
     def from_pretrained(cls, model_type):
         """Loads pretrained GPT-2 model weights from huggingface"""
@@ -161,5 +160,53 @@ class GPT(nn.Module):
     
 
 # ----------------------------------
-model = GPT.from_pretrained('gpt2')
-print("didn't crash yay!")
+# device assign
+device = 'cpu'
+if torch.cuda.is_available():
+    device = 'cuda'
+print(f"Using device: {device}")
+
+num_return_sequences = 5
+max_length = 30
+
+# model = GPT.from_pretrained('gpt2')
+model = GPT(GPTConfig()) # random model initialization
+model.eval()
+model.to('cuda')
+
+# prefix tokens
+import tiktoken
+enc = tiktoken.get_encoding('gpt2')
+tokens = enc.encode("Hello, I'm a language model,")
+tokens = torch.tensor(tokens, dtype=torch.long) # (8,)
+tokens = tokens.unsqueeze(0).repeat(num_return_sequences, 1) # (5, 8)
+x = tokens.to('cuda')
+
+# generate right now x is (B, T) where B = 5, T = 8
+# set the seed to 42
+torch.manual_seed(42)
+torch.cuda.manual_seed(42)
+while x.size(1) < max_length:  # 8 < 30
+    # forward the model to get the logits 
+    with torch.no_grad():
+        # 1. forward
+        logits = model(x) # (B, T, vocab_size)
+        # take the logits at last position 
+        logits = logits[:, -1, :] # (B, vocab_size) - (5, 50257)
+        # get the probabilites
+        probs = F.softmax(logits, dim=-1)
+        # do top-k smapling of 50 (hf pipeline default)
+        # topk_probs here becomes (5, 50), topk_indices is (5, 50)
+        topk_probs, topk_indices = torch.topk(probs, 50, dim=-1)
+        # select a token from the top-k probabilities
+        ix = torch.multinomial(topk_probs, 1) # (B, 1) - (5, 1)
+        # gather the corresponding indices
+        xcol = torch.gather(topk_indices, -1, ix) # (B, 1) - (5, 1)
+        # append to the sequence 
+        x = torch.cat((x, xcol), dim=1)
+
+# print the generated text
+for i in range(num_return_sequences):
+    tokens = x[i, :max_length].tolist()
+    decoded = enc.decode(tokens)
+    print(">", decoded)
